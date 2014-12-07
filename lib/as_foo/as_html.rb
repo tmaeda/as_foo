da2e9c9c9cd47f3d8e223c94d3730f6f08123140
Keita Urashima <ursm@ursm.jp>
2014-12-07 11:20:15 +0900

    Fix incorrect usage of named arguments

--- lib/as_foo/as_html.rb
+++ lib/as_foo/as_html.rb
@@ -1,13 +1,13 @@
 module AsFoo
   module AsHtml
-    def as_html(with: method = :w3m, options: options = nil)
-      case method
+    def as_html(with: :w3m, options: nil)
+      case with
       when :w3m
         IO.popen("w3m -dump -T text/html", "w") do |w3m|
           w3m.puts self.to_s
         end
       else
-        raise ArgumentError.new("unexpected method #{method}")
+        raise ArgumentError.new("unexpected method #{with}")
       end
     end
   end
--- lib/as_foo/as_markdown.rb
+++ lib/as_foo/as_markdown.rb
@@ -1,12 +1,12 @@
 module AsFoo
   module AsMarkdown
-    def as_markdown(with: method = :redcarpet, options: options = nil)
-      case method
+    def as_markdown(with: :redcarpet, options: nil)
+      case with
       when :redcarpet
         require 'redcarpet'
         Redcarpet::Markdown.new(Redcarpet::Render::HTML, options.to_h).render(self.to_s)
       else
-        raise ArgumentError.new("unexpected method #{method}")
+        raise ArgumentError.new("unexpected method #{with}")
       end
     end
   end
