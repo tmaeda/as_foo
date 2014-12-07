5b217f485ba0c854a431410806f529f4ee852886
Keita Urashima <ursm@ursm.jp>
2014-12-07 11:15:10 +0900

    Add lynx, links and elinks support to `#as_html`

--- lib/as_foo/as_html.rb
+++ lib/as_foo/as_html.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 module AsFoo
   module AsHtml
     def as_html(with: :w3m, options: nil)
@@ -5,6 +7,20 @@ module AsFoo
       when :w3m
         IO.popen("w3m -dump -T text/html", "w") do |w3m|
           w3m.puts self.to_s
+        end
+      when :lynx
+        IO.popen("lynx -dump -nonumbers -nolist -stdin", "w") do |lynx|
+          lynx.puts self.to_s
+        end
+      when :links
+        Tempfile.open ["as_foo", ".html"] do |src|
+          src.puts self.to_s
+
+          system "links -dump #{src.path}"
+        end
+      when :elinks
+        IO.popen("elinks -dump -no-numbering -no-references", "w") do |elinks|
+          elinks.puts self.to_s
         end
       else
         raise ArgumentError.new("unexpected method #{with}")
